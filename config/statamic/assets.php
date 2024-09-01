67575409d2a5234267c675126b1913513b3fd950
Nanosch Adams <adams@nanosch.de>
2024-09-01 11:39:30 +0200

    add landmatic

--- config/statamic/assets.php
+++ config/statamic/assets.php
@@ -43,21 +43,6 @@ return [
 
         /*
         |--------------------------------------------------------------------------
-        | Additional Image Extensions
-        |--------------------------------------------------------------------------
-        |
-        | Define any additional image file extensions you would like Statamic to
-        | process. You should ensure that both your server and the selected
-        | image manipulation driver properly supports these extensions.
-        |
-        */
-
-        'additional_extensions' => [
-            // 'heic',
-        ],
-
-        /*
-        |--------------------------------------------------------------------------
         | Save Cached Images
         |--------------------------------------------------------------------------
         |
@@ -67,23 +52,8 @@ return [
         |
         */
 
-        'cache' => false,
+        'cache' => env('SAVE_CACHED_IMAGES', false),
         'cache_path' => public_path('img'),
-
-        /*
-        |--------------------------------------------------------------------------
-        | Image Manipulation Defaults
-        |--------------------------------------------------------------------------
-        |
-        | You may define global defaults for all manipulation parameters, such as
-        | quality, format, and sharpness. These can and will be be overwritten
-        | on the tag parameter level as well as the preset level.
-        |
-        */
-
-        'defaults' => [
-            // 'quality' => 50,
-        ],
 
         /*
         |--------------------------------------------------------------------------
@@ -92,27 +62,25 @@ return [
         |
         | Rather than specifying your manipulation params in your templates with
         | the glide tag, you may define them here and reference their handles.
-        | They may also be automatically generated when you upload assets.
-        | Containers can be configured to warm these caches on upload.
+        | They will also be automatically generated when you upload assets.
         |
         */
 
         'presets' => [
-            // 'small' => ['w' => 200, 'h' => 200, 'q' => 75, 'fit' => 'crop'],
+            'xs-webp' => ['w' => 320, 'h' => 10000, 'q' => 85, 'fit' => 'contain', 'fm' => 'webp'],
+            'sm-webp' => ['w' => 480, 'h' => 10000, 'q' => 85, 'fit' => 'contain', 'fm' => 'webp'],
+            'md-webp' => ['w' => 768, 'h' => 10000, 'q' => 85, 'fit' => 'contain', 'fm' => 'webp'],
+            'lg-webp' => ['w' => 1280, 'h' => 10000, 'q' => 85, 'fit' => 'contain', 'fm' => 'webp'],
+            'xl-webp' => ['w' => 1440, 'h' => 10000, 'q' => 95, 'fit' => 'contain', 'fm' => 'webp'],
+            '2xl-webp' => ['w' => 1680, 'h' => 10000, 'q' => 95, 'fit' => 'contain', 'fm' => 'webp'],
+            'xs' => ['w' => 320, 'h' => 10000, 'q' => 85, 'fit' => 'contain'],
+            'sm' => ['w' => 480, 'h' => 10000, 'q' => 85, 'fit' => 'contain'],
+            'md' => ['w' => 768, 'h' => 10000, 'q' => 85, 'fit' => 'contain'],
+            'lg' => ['w' => 1280, 'h' => 10000, 'q' => 85, 'fit' => 'contain'],
+            'xl' => ['w' => 1440, 'h' => 10000, 'q' => 95, 'fit' => 'contain'],
+            '2xl' => ['w' => 1680, 'h' => 10000, 'q' => 95, 'fit' => 'contain'],
+            'og_image' => ['w' => 1200, 'h' => 630, 'q' => 95, 'fit' => 'contain'],
         ],
-
-        /*
-        |--------------------------------------------------------------------------
-        | Generate Image Manipulation Presets on Upload
-        |--------------------------------------------------------------------------
-        |
-        | By default, presets will be automatically generated on upload, ensuring
-        | the cached images are available when they are first used. You may opt
-        | out of this behavior here and have the presets generated on demand.
-        |
-        */
-
-        'generate_presets_on_upload' => true,
 
     ],
 
@@ -122,7 +90,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Enabling this will make Glide automatically crop assets at their focal
-    | point (which is the center if no focal point is defined). Otherwise,
+    | point (at at the center if no focal point is defined). Otherwise,
     | you will need to manually add any crop related parameters.
     |
     */
@@ -174,32 +142,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Focal Point Editor
-    |--------------------------------------------------------------------------
-    |
-    | When editing images in the Control Panel, there is an option to choose
-    | a focal point. When working with third-party image providers such as
-    | Cloudinary it can be useful to disable Statamic's built-in editor.
-    |
-    */
-
-    'focal_point_editor' => true,
-
-    /*
-    |--------------------------------------------------------------------------
-    | Enforce Lowercase Filenames
-    |--------------------------------------------------------------------------
-    |
-    | Control whether asset filenames will be converted to lowercase when
-    | uploading and renaming. This can help you avoid file conflicts
-    | when working in case-insensitive filesystem environments.
-    |
-    */
-
-    'lowercase' => true,
-
-    /*
-    |--------------------------------------------------------------------------
     | Additional Uploadable Extensions
     |--------------------------------------------------------------------------
     |
@@ -208,19 +150,8 @@ return [
     |
     */
 
-    'additional_uploadable_extensions' => [],
-
-    /*
-    |--------------------------------------------------------------------------
-    | SVG Sanitization
-    |--------------------------------------------------------------------------
-    |
-    | Statamic will automatically sanitize SVG files when uploaded to avoid
-    | potential security issues. However, if you have a valid reason for
-    | disabling this, and you trust your users, you may do so here.
-    |
-    */
-
-    'svg_sanitization_on_upload' => true,
+    'additional_uploadable_extensions' => [
+        'ico',
+    ],
 
 ];
